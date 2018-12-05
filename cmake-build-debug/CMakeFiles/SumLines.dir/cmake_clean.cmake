4a81c4b2988af8415b64f6a999e5796650bab13b
victorsmits <victor.smits@student.vinci.be>
2018-12-05 09:56:48 +0100

    up

---
+++ cmake-build-debug/CMakeFiles/SumLines.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SumLines.dir/main.c.o"
+  "SumLines.pdb"
+  "SumLines"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SumLines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
